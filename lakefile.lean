3e7965bc2248476700d6c17edcd67bd8a283c7a4
David-harmony <newproblemsolving@gmail.com>
2025-01-21 21:56:52 +0800

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -9,3 +9,17 @@ package «lean4-example» {
 lean_lib «Lean4Example» {
   -- add library configuration options here
 }
+
+
+mathlib = "nightly"
+
+[leanOptions]
+pp.unicode.fun = true # pretty-prints `fun a ↦ b`
+autoImplicit = false
+
+[[require]]
+name = "mathlib"
+scope = "leanprover-community"
+
+[[lean_lib]]
+name = "LeanMathlib"
